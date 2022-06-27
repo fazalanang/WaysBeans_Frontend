12fed5e81ef258de4f8aec6afe10a640ca6b9b8b
Your Name <faza.lanangdjati@gmail.com>
2022-06-27 23:18:59 +0700

    deploy netlify

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const baseUrl =
-process.env.REACT_APP_SERVER_URL ||
-'https://waysbeans34.herokuapp.com/api/v1' ||
-'https://localhost:5000/api/v1';
+export const API = axios.create({
+  baseURL:
+  process.env.SERVER_URL ||
+    "https://waysbeans34.herokuapp.com/api/v1" ||
+    "https://localhost:5000/api/v1",
+});
 
 export const setAuthToken = (token) => {
   if (token) {
